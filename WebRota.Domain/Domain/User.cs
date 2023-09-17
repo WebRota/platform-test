c080a224baa2d92cc3d2b54a793e730e5ce07a10
Willian <willian95prado@gmail.com>
2023-09-17 08:52:00 -0300

    Iniciando arquetura DDD

---
+++ WebRota.Domain/Domain/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRota.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este Campo é Obrigatorio!")]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public string Phone { get; set; }
+
+        public string cpf { get; set; }
+
+    }
+}
---
+++ WebRota.Infra/Context/WebRotaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRota.Infra.Context
+{
+    public class WebRotaContext : DbContext
+    {
+    }
+}
